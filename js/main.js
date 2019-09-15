1014ae31987189c929f01776eb24dd8bc0550743
mateusz0dziubinski <mateusz0dziubinski@o2.pl>
2019-09-15 19:32:21 +0200

    Zadanie: zrobione

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function mojaFunkcja(){
+    var numberX =prompt("Podaj pierwszą liczbę");
+    var numberY =prompt("Podaj drugą liczbę");
+    if (numberX, numberY){
+        console.log("Liczba 1 to" + " "+ numberX);
+        console.log("Liczba 2 to" + " "+ numberY);
+        }
+    }
